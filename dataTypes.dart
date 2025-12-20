0d3c9c6a9866e9b7ef677870a7ec3819a649062b
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-12-20 20:28:31 +0600

    first commit

---
+++ dataTypes.dart
@@ -0,0 +1,13 @@
+void main() 
+{
+    // explicit type
+    String name = "Shoruv";
+    int age = 21;
+    double height = 5.9;
+    bool isMale = true;
+
+    // can be used var too
+    var name2 = "Shoruv";
+    name2 = 21;
+    print(name2);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+So this is the first step of learning dart language. So I am learning it from YouTube channel.
+
+Today is 20th December 2025, Sunday. 7:58 PM
+
